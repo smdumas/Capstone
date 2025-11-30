9c9b3f13aa787a5ac0a4e49037b447ba97cb8c05
sdumas <smmckeever2013@gmail.com>
2025-11-30 15:40:15 -0600

    HWCapstone_SM

---
+++ components/footer.js
@@ -0,0 +1,8 @@
+import html from "html-literal";
+
+export default () => html`
+
+<footer>
+    "Created by SDMC"
+  </footer>
+  `;
---
+++ store/viewNotFound.js
@@ -0,0 +1,3 @@
+export default {
+  header: "View Not Found - 404", view: "viewNotFound"
+};
